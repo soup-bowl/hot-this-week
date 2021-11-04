5c6ba3d4c952722c6255a61ad63e8086fd028399
soup-bowl <code@soupbowl.io>
2021-11-04 16:58:27 +0000

    Added atribution.

--- main.php
+++ main.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Grabs details about a user from last.fm and posts a collage on their Twitter.
+ *
+ * @package lastfm-twitter
+ * @author soup-bowl <code@soupbowl.io>
+ * @license MIT
+ */
 
 require __DIR__ . '/vendor/autoload.php';
 
