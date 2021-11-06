460a1400a9e76cd4804efba1b246fa9a9d140b5f
soup-bowl <code@soupbowl.io>
2021-11-06 12:41:06 +0000

    Rolling in unit tests.

---
+++ tests/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Grabs details about a user from last.fm and posts a collage on their Twitter.
+ *
+ * @package lastfm-twitter
+ * @author soup-bowl <code@soupbowl.io>
+ * @license MIT
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
