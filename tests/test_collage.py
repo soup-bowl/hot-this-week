6d1b74afebca78f4ebb5a2822f12bef0a43287c3
soup-bowl <code@soupbowl.io>
2021-12-22 15:24:54 +0000

    Added Collage generation test.

--- tests/test_collage.py
+++ tests/test_collage.py
@@ -1,11 +1,24 @@
 from htw.collage import collage
 
-import unittest
+import unittest, os
 
 class StatChecks(unittest.TestCase):
 	@classmethod
 	def setUpClass(self):
-		self.stats = collage()
+		self.collage   = collage()
+		self.dummy_lfm = []
+		for i in range(5):
+			self.dummy_lfm.append({
+				"name": "Dummy",
+				"image": "https://source.unsplash.com/featured/?face,person",
+				"plays": "123",
+			})
+
+	def test_collage_generation(self):
+		"""Generates a collage using dummy data.
+		"""
+		collage = self.collage.new(self.dummy_lfm, True)
+		self.assertTrue(os.path.exists(collage))
 
 	def test_name_concatenation(self):
 		"""Tests the concatentation function returns expected results for okay and too-long names.
@@ -13,5 +26,5 @@ class StatChecks(unittest.TestCase):
 		name_okay = "a" * 15
 		name_conc = name_okay + "a"
 
-		self.assertEqual(self.stats.cut_long_artist_name(name_okay), name_okay)
-		self.assertEqual(self.stats.cut_long_artist_name(name_conc), "aaaaaaaaaaaa...")
+		self.assertEqual(self.collage.cut_long_artist_name(name_okay), name_okay)
+		self.assertEqual(self.collage.cut_long_artist_name(name_conc), "aaaaaaaaaaaa...")
