144211e855761658b31d8cc6a4718d8e1761c77e
soup-bowl <code@soupbowl.io>
2024-07-29 07:43:18 +0000

    Added check to update poetry.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 pipx install poetry
+poetry self update
 poetry install
 echo ${HTW_CONFIG} | jq > config.json
 echo "Run the command by doing executing 'poetry run python -m htw -h'."
